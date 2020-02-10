55c25c42bbe023efab35e4436ba5288c1ad5b26f
Tommy Mallis <tommymallis@gmail.com>
2020-02-09 20:14:32 -0800

    Added menu and title, using w3

---
+++ js.js
@@ -0,0 +1,11 @@
+/**
+ * Functions for opening and closing the hamburger menu on mobile
+ * **/
+
+function w3_open() {
+  document.getElementById('mySidebar').style.display = 'block';
+}
+
+function w3_close() {
+  document.getElementById('mySidebar').style.display = 'none';
+}
--- sketch.js
+++ sketch.js
@@ -1,17 +1,26 @@
+/* Setup the canvas for drawing, using P5 js */
 setup = () => {
   createCanvas(window.innerWidth, window.innerHeight);
 };
 
+/*Will Draw when you hold the mouse button down */
+/*Will erase when you hold e on keyboard */
 draw = () => {
   if (mouseIsPressed) {
-    strokeWeight(5);
-    stroke(0);
-    line(mouseX, mouseY, pmouseX, pmouseY);
+    drawLine();
   } else if (keyIsPressed && key == 'e') {
     drawEraser();
   }
 };
 
+//Function for drawing style
+const drawLine = () => {
+  strokeWeight(5);
+  stroke(0);
+  line(mouseX, mouseY, pmouseX, pmouseY);
+};
+
+//Function for erasing style
 const drawEraser = () => {
   strokeWeight(100);
   stroke(255);
