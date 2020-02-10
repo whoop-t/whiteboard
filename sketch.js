68c73355afd0041032b4b3eb949371270821c969
Tommy Mallis <tommymallis@gmail.com>
2020-02-09 18:03:34 -0800

    first commit

---
+++ sketch.js
@@ -0,0 +1,12 @@
+setup = () => {
+  createCanvas(window.innerWidth, window.innerHeight);
+};
+
+draw = () => {
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
+};
